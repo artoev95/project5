ae9f7319750a93e6f8ab56dce59daf9f6a25b978
artoev95 <arthovs94@hotmail.com>
2017-02-25 15:51:35 +0000

    update

--- touristapp.js
+++ touristapp.js
@@ -1,11 +1,26 @@
+'use strict';
 var express = require('express');
 var bodyParser = require('body-parser');
+var stormpath = require('express-stormpath');
 
 var app = express();
 
-app.use(bodyParser.json());
+app.use(stormpath.init(app,{
+	client: {
+		apiKey: {
+			id:'K65PWVYXGUD27P7ZMNLWZW9WXapiKey',
+			secret:'jVUFqUhpcuJile8YVvgWbXzDQSWhixGuwTSz3g62aCA'
+		}
+		},
+		application: {
+			href:'https://api.stormpath.com/v1/applications/4RFzDtSJdugl5Vu8vWqpQM'
 
-app.get('/',function (request, response) {
+		}
+	}
+	));
+app.use(bodyParser.json());
+app
+app.get('/api/planner', stormpath.apiAuthenticationRequired, function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("We're up and running!!!");
 });
@@ -17,7 +32,9 @@ app.get('/api/planner',plan.index);
 app.post('/api/planner',plan.create);
 app.put('/api/planner/:id',plan.update);
 app.delete('/api/planner/:id',plan.delete);
-app.listen(8000)
 
+app.on('stormpath.ready',function() {
+app.listen(8000);
+});
 console.log("Server running at http://127.0.0.1:8000/");
-//laura soset moi xui
+//
