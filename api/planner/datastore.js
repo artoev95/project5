7b0698bd1416f64da3eabbcecc6a7889e5566084
artoev95 <arthovs94@hotmail.com>
2017-03-11 19:51:43 +0000

    update

--- api/planner/datastore.js
+++ api/planner/datastore.js
@@ -1,4 +1,4 @@
 exports.planner = [
-{id: 'a4vhAoFG', location: "Joe Bloggs", attraction: "1 Bank Lane", approx_cost: "051-123456 "},
- {id: 'hwX6aOr7', location: "Ellen Bliggs", attraction: "2 River Road", approx_cost: "051-123457"}
+{id: 'a4vhAoFG', location: "Marsielle", attraction: "Beach", approx_cost: "450 euros ",holiday_type :"summer",transport_type :"the road"},
+ {id: 'hwX6aOr7', location: "Swizterland", attraction: "Mountains", approx_cost: "950 euros",holiday_type:"winter",transport_type :"sea"}
 ]
--- touristapp.js
+++ touristapp.js
@@ -1,26 +1,37 @@
-'use strict';
-var express = require('express');
+'use strict'
+
+const express = require('express')
+const fs = require('fs')
+const https =require('https')
+const http =require('http')
+const path = require('path')
+
+const app = express()
+const directoryToServe = 'client'
+const port = 3443
+
+app.use('/',express.static(path.join(__dirname,'..',directoryToServe)))
+
+const httpsOptions = {
+  cert: fs.readFileSync(path.join(__dirname,'ssl','server.crt')),
+  key: fs.readFileSync(path.join(__dirname,'ssl','server.key'))
+}
+
+https.createServer(httpsOptions, app)
+.listen(port, function()
+{
+  console.log(`Serving the ${directoryToServe}/directory at https://localhost:${port}`)})
+
 var bodyParser = require('body-parser');
-var stormpath = require('express-stormpath');
 
-var app = express();
 
-app.use(stormpath.init(app,{
-	client: {
-		apiKey: {
-			id:'K65PWVYXGUD27P7ZMNLWZW9WXapiKey',
-			secret:'jVUFqUhpcuJile8YVvgWbXzDQSWhixGuwTSz3g62aCA'
-		}
-		},
-		application: {
-			href:'https://api.stormpath.com/v1/applications/4RFzDtSJdugl5Vu8vWqpQM'
 
-		}
-	}
-	));
+
+
+
 app.use(bodyParser.json());
 app
-app.get('/api/planner', stormpath.apiAuthenticationRequired, function(request, response) {
+app.get('/',  function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("We're up and running!!!");
 });
@@ -33,8 +44,8 @@ app.post('/api/planner',plan.create);
 app.put('/api/planner/:id',plan.update);
 app.delete('/api/planner/:id',plan.delete);
 
-app.on('stormpath.ready',function() {
-app.listen(8000);
-});
+
+
+
 console.log("Server running at http://127.0.0.1:8000/");
 //
