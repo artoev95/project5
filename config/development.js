77473a24be5c476532d4f1c3d33150bd74fc936b
artoev95 <arthovs94@hotmail.com>
2017-04-25 11:00:14 +0100

    added new file

---
+++ config/development.js
@@ -0,0 +1,9 @@
+'use strict';
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://hui12345:hui12345@ds161179.mlab.com:61179/hui12345'
+  }
+};
